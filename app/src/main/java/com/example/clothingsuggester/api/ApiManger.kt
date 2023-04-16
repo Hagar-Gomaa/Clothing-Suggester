5f539b0282be46fb007e9f57b8e706aa02ca59d2
Hagar-Gomaa <hagargomaa313@gmail.com>
2023-04-16 20:06:40 +0200

    add api manger

---
+++ app/src/main/java/com/example/clothingsuggester/api/ApiManger.kt
@@ -0,0 +1,19 @@
+package com.example.clothingsuggester.api
+
+import android.util.Log
+import com.example.clothingsuggester.R
+import com.example.clothingsuggester.models.WeatherResponse
+import com.example.clothingsuggester.ui.ImagesViewPagerAdapter
+import com.google.gson.Gson
+import okhttp3.*
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
+import java.io.IOException
+
+class ApiManger {
+
+
+
+     }
+
+
+
